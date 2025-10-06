021c4c94097202cb899691534597b436f83c3941
Zeek Zone <74927544+Zeek-Zone@users.noreply.github.com>
2025-10-06 20:55:17 +0100

    Update tp2.php

--- tp1/tp2.php
+++ tp1/tp2.php
@@ -45,6 +45,8 @@ echo "Exercice 2: <br>";
 $X = 15;
 $n = 10;
 
+// avec for
+echo "2) avec for: <br>";
 // calcul de X^n
 $Xn = 1;
 for ($i = 1; $i <= $n; $i++) {
@@ -52,4 +54,69 @@ for ($i = 1; $i <= $n; $i++) {
 }
 
 echo "La valeur de ". $X . "^" . $n . " est: " . $Xn . "<br>";
+
+// avec while
+echo "1) avec while: <br>";
+// calcul de X^n
+$Xn = 1;
+$i = 1;
+while ($i <= $n) {
+    $Xn *= $X; // $Xn = $Xn * $X;
+    $i++;
+}
+echo "La valeur de ". $X . "^" . $n . " est: " . $Xn . "<br>";
+
+echo "<hr>";
+echo "Exercice 3: <br>";
+// Declaration et initialisation 
+$N = 10;
+
+// avec for
+echo "2) avec for: <br>";
+$S = 0;
+for ($i = 1; $i <= $N; $i++) {
+    $S += $i; // $S = $S + $i;
+}
+echo "La somme de 1 à " . $N . " est: " . $S . "<br>";
+
+// avec while
+echo "1) avec while: <br>";
+$S = 0;
+
+$i = 1; // initialisation de i
+while ($i <= $N) { // condition d'arret
+    $S += $i; // $S = $S + $i;
+    $i++; // incrémentation
+}
+
+echo "La somme de 1 à " . $N . " est: " . $S . "<br>";
+
+echo "<hr>";
+echo "Exercice 4: <br>";
+// Affichage des lettres de l'alphabet avec la fonction de conversion ASCII
+$i = 97; // code ASCII de a
+while ($i <= 122) {
+    echo chr($i) . "<br>";
+    $i++;
+}
+echo "<hr>";
+// Avec un affihchage formaté printf
+$i = 65; // code ASCII de A
+printf("CODE ASCII: Lettre <br>");
+while ($i <= 90) {
+    printf("%d: %c <br>", $i, $i);
+    $i++;
+}
+echo "<hr>";
+// avec for
+echo "2) avec for: <br>";
+for ($i = 65; $i <= 90; $i++) {
+    printf("%d: %c <br>", $i, $i);
+}
+
+echo "<hr>";
+
+for ($i = 65; $i <= 90; $i++) {
+    echo chr($i) . "<br>";
+}
 ?>
